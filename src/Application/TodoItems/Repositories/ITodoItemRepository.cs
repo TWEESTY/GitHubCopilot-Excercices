76caa200af5228f63d814c00ee637cfcc698cc82
TWEESTY <chausse.nicolas@gmail.com>
2025-02-01 17:34:39 +0100

    Add some code to further exercices

--- src/Application/TodoItems/Repositories/ITodoItemRepository.cs
+++ src/Application/TodoItems/Repositories/ITodoItemRepository.cs
@@ -7,7 +7,7 @@ namespace Copilot.Application.TodoItems.Repositories;
 public interface ITodoItemRepository
 {
     public TodoItem? Get(int id);
-    public PaginatedList<TodoItem> GetList(int? pageNumber, int? pageSize);
+    public PaginatedList<TodoItem> GetList(int? pageNumber = null, int? pageSize = null);
 
     public TodoItem Create(TodoItem entity);
     public TodoItem Update(TodoItem entity);
