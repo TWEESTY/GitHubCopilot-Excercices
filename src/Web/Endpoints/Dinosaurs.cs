84903e0212adcbce4a4356dcb3097328b64cddeb
Nicolas Chaussé <nicolas.chausse@external.ten.com>
2025-02-14 11:36:45 +0100

    -

--- src/Web/Endpoints/Dinosaurs.cs
+++ src/Web/Endpoints/Dinosaurs.cs
@@ -8,8 +8,15 @@ using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Copilot.Web.Endpoints;
 
+/// <summary>
+/// Represents the endpoint group for managing dinosaurs.
+/// </summary>
 public class Dinosaurs : EndpointGroupBase
 {
+    /// <summary>
+    /// Maps the endpoints for managing dinosaurs.
+    /// </summary>
+    /// <param name="app">The web application.</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -20,24 +27,48 @@ public class Dinosaurs : EndpointGroupBase
             .MapDelete(DeleteDinosaur, "{id}");
     }
 
+    /// <summary>
+    /// Gets the list of dinosaurs.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <returns>The list of dinosaurs.</returns>
     public async Task<Ok<List<Dinosaur>>> GetDinosaurs(ISender sender)
     {
         var result = await sender.Send(new GetDinosaursQuery());
         return TypedResults.Ok(result);
     }
 
+    /// <summary>
+    /// Gets a dinosaur by ID.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="id">The ID of the dinosaur.</param>
+    /// <returns>The dinosaur if found, otherwise NotFound.</returns>
     public async Task<Results<Ok<Dinosaur>, NotFound>> GetDinosaur(ISender sender, int id)
     {
         var result = await sender.Send(new GetDinosaurQuery(id));
         return result is not null ? TypedResults.Ok(result) : TypedResults.NotFound();
     }
 
+    /// <summary>
+    /// Creates a new dinosaur.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="command">The create dinosaur command.</param>
+    /// <returns>The ID of the created dinosaur.</returns>
     public async Task<Created<int>> CreateDinosaur(ISender sender, CreateDinosaurCommand command)
     {
         var createdDinosaur = await sender.Send(command);
         return TypedResults.Created($"/{nameof(Dinosaurs)}/{createdDinosaur.Id}", createdDinosaur.Id);
     }
 
+    /// <summary>
+    /// Updates a dinosaur.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="id">The ID of the dinosaur.</param>
+    /// <param name="command">The update dinosaur command.</param>
+    /// <returns>NoContent if successful, otherwise BadRequest.</returns>
     public async Task<Results<NoContent, BadRequest>> UpdateDinosaur(ISender sender, int id, UpdateDinosaurCommand command)
     {
         if (id != command.Id) return TypedResults.BadRequest();
@@ -46,6 +77,12 @@ public class Dinosaurs : EndpointGroupBase
         return TypedResults.NoContent();
     }
 
+    /// <summary>
+    /// Deletes a dinosaur.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="id">The ID of the dinosaur.</param>
+    /// <returns>NoContent if successful.</returns>
     public async Task<NoContent> DeleteDinosaur(ISender sender, int id)
     {
         await sender.Send(new DeleteDinosaurCommand(id));
--- src/Web/Endpoints/TodoItems.cs
+++ src/Web/Endpoints/TodoItems.cs
@@ -10,6 +10,10 @@ namespace Copilot.Web.Endpoints;
 
 public class TodoItems : EndpointGroupBase
 {
+    /// <summary>
+    /// Maps the endpoints for the TodoItems group.
+    /// </summary>
+    /// <param name="app">The WebApplication instance.</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -20,6 +24,12 @@ public class TodoItems : EndpointGroupBase
             .MapDelete(DeleteTodoItem, "{id}");
     }
 
+    /// <summary>
+    /// Retrieves a paginated list of TodoItems.
+    /// </summary>
+    /// <param name="sender">The sender instance.</param>
+    /// <param name="query">The query parameters for retrieving TodoItems.</param>
+    /// <returns>A response containing the paginated list of TodoItems.</returns>
     public async Task<Ok<PaginatedList<TodoItemBriefDto>>> GetTodoItemsWithPagination(ISender sender, [AsParameters] GetTodoItemsWithPaginationQuery query)
     {
         var result = await sender.Send(query);
@@ -27,6 +37,12 @@ public class TodoItems : EndpointGroupBase
         return TypedResults.Ok(result);
     }
 
+    /// <summary>
+    /// Creates a new TodoItem.
+    /// </summary>
+    /// <param name="sender">The sender instance.</param>
+    /// <param name="command">The command for creating a TodoItem.</param>
+    /// <returns>A response containing the ID of the created TodoItem.</returns>
     public async Task<Created<int>> CreateTodoItem(ISender sender, CreateTodoItemCommand command)
     {
         var id = await sender.Send(command);
@@ -34,6 +50,13 @@ public class TodoItems : EndpointGroupBase
         return TypedResults.Created($"/{nameof(TodoItems)}/{id}", id);
     }
 
+    /// <summary>
+    /// Updates a TodoItem.
+    /// </summary>
+    /// <param name="sender">The sender instance.</param>
+    /// <param name="id">The ID of the TodoItem to update.</param>
+    /// <param name="command">The command for updating the TodoItem.</param>
+    /// <returns>A response indicating the success or failure of the update operation.</returns>
     public async Task<Results<NoContent, BadRequest>> UpdateTodoItem(ISender sender, int id, UpdateTodoItemCommand command)
     {
         if (id != command.Id) return TypedResults.BadRequest();
@@ -43,15 +66,28 @@ public class TodoItems : EndpointGroupBase
         return TypedResults.NoContent();
     }
 
+    /// <summary>
+    /// Updates the details of a TodoItem.
+    /// </summary>
+    /// <param name="sender">The sender instance.</param>
+    /// <param name="id">The ID of the TodoItem to update.</param>
+    /// <param name="command">The command for updating the TodoItem details.</param>
+    /// <returns>A response indicating the success or failure of the update operation.</returns>
     public async Task<Results<NoContent, BadRequest>> UpdateTodoItemDetail(ISender sender, int id, UpdateTodoItemDetailCommand command)
     {
         if (id != command.Id) return TypedResults.BadRequest();
-        
+
         await sender.Send(command);
-        
+
         return TypedResults.NoContent();
     }
 
+    /// <summary>
+    /// Deletes a TodoItem.
+    /// </summary>
+    /// <param name="sender">The sender instance.</param>
+    /// <param name="id">The ID of the TodoItem to delete.</param>
+    /// <returns>A response indicating the success or failure of the delete operation.</returns>
     public async Task<NoContent> DeleteTodoItem(ISender sender, int id)
     {
         await sender.Send(new DeleteTodoItemCommand(id));
