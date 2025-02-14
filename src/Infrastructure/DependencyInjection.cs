44677cee02908685f077b75821b31f49bbb65e04
Nicolas Chaussé <nicolas.chausse@external.ten.com>
2025-02-14 11:25:51 +0100

    -

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Copilot.Application.Dinosaurs.Repositories;
+﻿using System.IO;
+using Copilot.Application.Dinosaurs.Repositories;
 using Copilot.Application.TodoItems.Repositories;
 using Copilot.Domain.Constants;
 using Copilot.Domain.Entities;
@@ -18,7 +19,7 @@ public static class DependencyInjection
     {
         builder.Services.AddSingleton(TimeProvider.System);
 
-        builder.Services.AddSingleton<ITodoItemRepository>(provider => 
+        builder.Services.AddSingleton<ITodoItemRepository>(provider =>
             new TodoItemRepository(todoItems: CreateTodoItemsForSeeding())
         );
 
@@ -27,49 +28,47 @@ public static class DependencyInjection
         );
     }
 
-    private static List<TodoItem> CreateTodoItemsForSeeding(){
+    private static List<TodoItem> CreateTodoItemsForSeeding()
+    {
         return new List<TodoItem>{
-            new TodoItem{
-                Id = 1,
-                Done = true,
-                Note = "First item",
-                Priority = PriorityLevel.High,
-                Reminder = null,
-                Title = "First item title"
-            },
-            new TodoItem{
-                Id = 2,
-                Done = false,
-                Note = "Second item",
-                Priority = PriorityLevel.Low,
-                Reminder = DateTime.Today,
-                Title = "Second item title"
-            }
-        };
+                    new TodoItem{
+                        Id = 1,
+                        Done = true,
+                        Note = "First item",
+                        Priority = PriorityLevel.High,
+                        Reminder = null,
+                        Title = "First item title"
+                    },
+                    new TodoItem{
+                        Id = 2,
+                        Done = false,
+                        Note = "Second item",
+                        Priority = PriorityLevel.Low,
+                        Reminder = DateTime.Today,
+                        Title = "Second item title"
+                    }
+                };
     }
 
     private static List<Dinosaur> CreateDinosaursForSeeding()
     {
-        return new List<Dinosaur>
-               {
-                   new Dinosaur
-                   {
-                       Id = 1,
-                       Name = "T-Rex",
-                       Species = "Tyrannosaurus",
-                       Sex = "Male",
-                       CountryOfOrigin = "USA",
-                       NumberOfScales = 1000
-                   },
-                   new Dinosaur
-                   {
-                       Id = 2,
-                       Name = "Velociraptor",
-                       Species = "Velociraptor",
-                       Sex = "Female",
-                       CountryOfOrigin = "Mongolia",
-                       NumberOfScales = 500
-                   }
-               };
+        var dinosaurs = new List<Dinosaur>();
+        var csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Dinosaurs.csv");
+        var lines = File.ReadAllLines(csvPath).Skip(1);
+        foreach (var line in lines)
+        {
+            var values = line.Split(',');
+            var dinosaur = new Dinosaur
+            {
+                Id = int.Parse(values[0]),
+                Name = values[1],
+                Species = values[2],
+                Sex = values[3],
+                CountryOfOrigin = values[4],
+                NumberOfScales = int.Parse(values[5])
+            };
+            dinosaurs.Add(dinosaur);
+        }
+        return dinosaurs;
     }
 }
